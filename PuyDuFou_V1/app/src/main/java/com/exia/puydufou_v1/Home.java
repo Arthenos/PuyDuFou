e0934e5f3a806749a15b08b309409159d41364f0
maxim_000 <maxime.rifflart@viacesi.fr>
2015-06-15 14:25:25 +0200

    Choibedoule

--- PuyDuFou_V1/app/src/main/java/com/exia/puydufou_v1/Home.java
+++ PuyDuFou_V1/app/src/main/java/com/exia/puydufou_v1/Home.java
@@ -13,6 +13,8 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         // Coucou
         // Coucou 2 d2
+        // Coucou Flo
+
     }
 
     @Override
